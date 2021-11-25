3f5b6d94f159f52bc46d84ab097467147114a470
hyunil <thdgusdddd@gmail.com>
2021-11-25 16:28:29 +0900

    change1

--- movie/app/admin.py
+++ movie/app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Movie_info
+from .models import Screen_info
+from .models import Screen
+from .models import Customer_Pesonal_info
+from .models import Reservation
+from .models import Employer_Personal_info
+
+admin.site.register(Movie_info)
+admin.site.register(Screen_info)
+admin.site.register(Screen)
+admin.site.register(Customer_Pesonal_info)
+admin.site.register(Reservation)
+admin.site.register(Employer_Personal_info)
